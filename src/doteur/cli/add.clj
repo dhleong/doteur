274692cc2b8c985e57aa452c24dc3012fa547f65
Daniel Leong <me@dhleong.net>
2023-06-19 14:37:49 -0400

    Support github paths as an alternate to full URIs for `doteur add`

--- src/doteur/cli/add.clj
+++ src/doteur/cli/add.clj
@@ -1,10 +1,19 @@
 (ns doteur.cli.add
   (:require
    [babashka.process :refer [shell]]
-   [doteur.cli.util :refer [print-formatted]]))
+   [doteur.cli.util :refer [print-formatted]]
+   [clojure.string :as str]))
+
+(defn- format-uri [uri]
+  (if (str/includes? uri ":")
+    ; Probably a full URI
+    uri
+
+    ; Github abbreviation
+    (str "git@github.com:" uri ".git")))
 
 (defn command [{{:keys [envs-dir uri dry-run]} :opts}]
-  (let [cmd ["git" "clone" uri envs-dir]]
+  (let [cmd ["git" "clone" (format-uri uri) envs-dir]]
     (when dry-run
       (print-formatted [:yellow "*** THIS IS A DRY RUN ***"]))
     (apply println cmd)
